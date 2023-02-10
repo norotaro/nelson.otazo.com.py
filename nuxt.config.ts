a5ecd191d353a75382ae98d8a68e7f664d005e9f
Nelson Otazo <nelson.otazo@gmail.com>
2023-02-10 17:23:32 -0300

    Nuxt is updated and Google Fonts is added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,10 @@
 export default defineNuxtConfig({
   modules: [
     "@nuxt/content",
-    "@nuxtjs/tailwindcss"
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts"
   ],
+  devServerHandlers: [],
   content: {
     // https://content.nuxtjs.org/api/configuration
     highlight: {
@@ -17,5 +19,13 @@ export default defineNuxtConfig({
   },
   tailwindcss: {
     cssPath: "~/assets/css/main.css",
+  },
+
+  // Google Fonts module configuration: https://google-fonts.nuxtjs.org
+  googleFonts: {
+    families: {
+      Montserrat: [100, 200, 300, 400, 500, 600, 700, 800, 900]
+    },
+    display: 'swap'
   },
 });
