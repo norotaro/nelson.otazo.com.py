8fe71383cad3d396836c03537aea381f4aa44317
Nelson Otazo <nelson.otazo@gmail.com>
2023-07-12 10:42:55 -0400

    nuxt-gtag is removed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,7 @@ export default defineNuxtConfig({
   modules: [
     "@nuxt/content",
     "@nuxtjs/tailwindcss",
-    "@nuxtjs/google-fonts",
-    'nuxt-gtag'
+    "@nuxtjs/google-fonts"
   ],
   devServerHandlers: [],
   content: {
