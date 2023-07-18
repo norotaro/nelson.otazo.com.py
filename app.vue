e566f8b53f942a026c795679843ba9e5bdbe32c1
Nelson Otazo <nelson.otazo@gmail.com>
2023-07-18 16:07:16 -0400

    removed google fonts module

--- app.vue
+++ app.vue
@@ -20,6 +20,19 @@ useHead({
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com',
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;500;600;700;800;900&display=swap'
+      },
+      {
         hid: 'canonical',
         rel: 'canonical',
         href: url
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,9 @@
 export default defineNuxtConfig({
   modules: [
     "@nuxt/content",
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/google-fonts"
+    "@nuxtjs/tailwindcss"
   ],
+  sourcemap: false,
   devServerHandlers: [],
   content: {
     // https://content.nuxtjs.org/api/configuration
@@ -18,17 +18,12 @@ export default defineNuxtConfig({
   tailwindcss: {
     cssPath: "~/assets/css/main.css",
   },
-
-  // Google Fonts module configuration: https://google-fonts.nuxtjs.org
-  googleFonts: {
-    families: {
-      Montserrat: [100, 200, 300, 400, 500, 600, 700, 800, 900]
-    },
-    display: 'swap'
-  },
   runtimeConfig: {
     public: {
       gtagId: ''
     }
-  }
+  },
+  site: {
+    url: 'https://nelson.otazo.com.py',
+  },
 });
